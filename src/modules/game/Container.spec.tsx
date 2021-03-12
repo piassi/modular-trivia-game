d754bc9de0f8be626083e80f2f81f59d37a2728b
Thales Nunes <thales.piassi@ifood.com.br>
2021-03-12 20:11:01 -0300

    test: ensure loads next trivia after user select answer

--- src/modules/game/Container.spec.tsx
+++ src/modules/game/Container.spec.tsx
@@ -18,17 +18,42 @@ describe('Container', () => {
   it('should load first trivia when user clicks on start button', async () => {
     renderWithReduxProvider(<Container />);
     jest.spyOn(service, 'getTrivias').mockResolvedValue(trivias);
-    const button = screen.getByRole('button', {
+    const startButton = screen.getByRole('button', {
       name: /start/i,
     });
 
-    user.click(button);
+    user.click(startButton);
 
     await waitFor(() => {
       const title = screen.queryByRole('heading', {
         name: trivias[0].category,
       });
       const question = screen.queryByText(trivias[0].question);
+
+      expect(title).toBeInTheDocument();
+      expect(question).toBeInTheDocument();
+    });
+  });
+
+  it('should load next trivia after user select answer', async () => {
+    renderWithReduxProvider(<Container />);
+    jest.spyOn(service, 'getTrivias').mockResolvedValue(trivias);
+    const startButton = screen.getByRole('button', {
+      name: /start/i,
+    });
+
+    user.click(startButton);
+
+    await waitFor(() => {
+      const answerTruthyButton = screen.getByRole('button', {
+        name: /true/i,
+      });
+      user.click(answerTruthyButton);
+
+      const title = screen.queryByRole('heading', {
+        name: trivias[1].category,
+      });
+      const question = screen.queryByText(trivias[1].question);
 
       expect(title).toBeInTheDocument();
       expect(question).toBeInTheDocument();
